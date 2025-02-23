cdb695f1ff7f6ff259161ffc2a527d1915b2c7ab
thalesgibbon <thales.gibbon@gmail.com>
2025-02-23 05:07:18 -0300

    add general sub

--- streaming_pipeline/infra/modules/pubsub/main.tf
+++ streaming_pipeline/infra/modules/pubsub/main.tf
@@ -4,6 +4,11 @@ resource "google_pubsub_topic" "backend_events_topic" {
   name = "backend-events-topic"
 }
 
+resource "google_pubsub_subscription" "general_subscription" {
+  name  = "backend-events-topic-sub"
+  topic = google_pubsub_topic.backend_events_topic.name
+}
+
 resource "google_pubsub_subscription" "order_subscription" {
   name  = "order-subscription"
   topic = google_pubsub_topic.backend_events_topic.name
